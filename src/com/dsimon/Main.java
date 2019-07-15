31feac753ddac7dea0ca12272ea68ed9f6dbfb44
Hurlyburly2 <dougdougmann@gmail.com>
2019-07-15 01:09:03 -0400

    lesson part1 done

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -1,8 +1,25 @@
 package com.dsimon;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Customer customer = new Customer("Doug", 54.96);
+        Customer anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+        System.out.println("Balance for customer " + customer.getName() + ": " + customer.getBalance());
+
+        // INSERTING INTO ARRAY CAN BE INTENSIVE BECAUSE EVERY ITEM NEEDS TO BE SHIFTED BY ONE
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        System.out.println("intList: " + intList.toString());
+
+        intList.add(1, 2);
+        System.out.println("intList: " + intList.toString());
     }
 }
